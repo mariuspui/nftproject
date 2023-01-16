f94a2287b8d5dc78586ed13a9de3d0e3efb29325
abdulrafaysiddiqui <abdulrafay@dechains.com>
2023-01-17 01:39:25 +0500

    added env variables in config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import env from "dotenv";
+
+env.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+    solidity: "0.8.17",
+    networks: {
+        eth: {
+            url: process.env.RPC_URL,
+            accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+        },
+    },
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_API_KEY,
+    },
 };
 
 export default config;
+
