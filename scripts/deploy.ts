f85c915d2973098b932dffa41f382226793e7bef
abdulrafaysiddiqui <abdulrafay@dechains.com>
2023-01-17 01:39:05 +0500

    updated deployment script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,14 @@
 import { ethers } from "hardhat";
+import { deploy } from "../utils";
 
 const TOKEN_BASE_URI = "";
 const FREE_MINTS = 500;
-const MINT_PRICE = ethers.utils.parseEther("0.1");
+const MINT_PRICE = ethers.utils.parseEther("0.1").toString();
 
 async function main() {
-    const NFT = await ethers.getContractFactory("NFT");
-    const nft = await NFT.deploy(TOKEN_BASE_URI, FREE_MINTS, MINT_PRICE);
-
-    await nft.deployed();
+    const nft = await deploy("NFT", [TOKEN_BASE_URI, FREE_MINTS, MINT_PRICE]);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
     console.error(error);
     process.exitCode = 1;
